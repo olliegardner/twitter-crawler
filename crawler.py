0c6b1ecd0fb5d864859d527c30de1f03c1686b4f
olliegardner <olliegardner@gmail.com>
2021-02-13 16:03:43 +0000

    added streaming for rugby related words

--- crawler.py
+++ crawler.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
+from listener import StreamListener
 
 import tweepy
 import os
+import time
 
 
 def connect_to_twitter():
@@ -19,17 +21,50 @@ def connect_to_twitter():
         print('Twitter authenitcation failed.')
         return False
 
-    user = api.get_user('POTUS')
+    return auth, api
 
-    print(user.name)
-    print(user.screen_name)
-    print(user.followers_count)
 
-    return True
+def create_stream(auth, api):
+    locations = [-10.392627, 49.681847, 1.055039, 61.122019]  # UK and Ireland
+
+    words = [
+        'Rugby', '6 Nations', 'Six Nations', 'Guinness Six Nations',
+        'Guinness 6 Nations', 'Murrayfield', 'BT Murrayfield', 'Twickenham',
+        'Principality Stadium', 'Aviva Stadium', 'Stade de France',
+        'Stadio Olimpico', 'Stuart Hogg', 'Finn Russell', 'Wayne Barnes',
+        'Scrum', 'Try', 'Penalty', 'Drop goal', 'Doddie Weird Cup', 'Scotland',
+        'Ireland', 'Wales', 'England', 'France', 'Italy'
+    ]
+
+    listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
+    streamer = tweepy.Stream(auth=auth, listener=listener)
+    streamer.filter(
+        locations=locations, track=words, languages=['en'], is_async=True
+    )
+
+    results = True
+    counter = 0
+    geo_code = '55.8554403,-4.3024977,50km'  # Glasgow geo code with 50km radius
+
+    while results:
+        if counter < 180:  # number of requests allowed for Twitter
+            try:
+                results = api.search(
+                    geocode=geo_code, count=100, lang="en", max_id=None
+                )
+            except Exception as e:
+                print(e)
+
+            counter += 1
+        else:
+            # sleep for 15 minutes to meet Twitter rate limit
+            time.sleep(15 * 60)
 
 
 if __name__ == '__main__':
     load_dotenv()
 
-    if connect_to_twitter():
-        print('success')
+    auth, api = connect_to_twitter()
+
+    if auth:
+        create_stream(auth, api)
